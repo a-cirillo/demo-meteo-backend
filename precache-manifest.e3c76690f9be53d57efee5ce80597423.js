0c6c987d8e107a91557bfba4bef6a17ac658678c
alvinator <a.cirillo@bitdrome.com>
2020-04-10 15:57:32 +0200

    Updates

---
+++ precache-manifest.e3c76690f9be53d57efee5ce80597423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299e73c9bdf25ff8fbe3e18ccba14843",
+    "url": "/demo-meteo-backend/index.html"
+  },
+  {
+    "revision": "77d7670aaf1afce2661d",
+    "url": "/demo-meteo-backend/static/css/main.902adcb6.chunk.css"
+  },
+  {
+    "revision": "6ade8505d472e24e0d8a",
+    "url": "/demo-meteo-backend/static/js/2.e152529a.chunk.js"
+  },
+  {
+    "revision": "74302187dc1f033042b1164070a401c8",
+    "url": "/demo-meteo-backend/static/js/2.e152529a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d7670aaf1afce2661d",
+    "url": "/demo-meteo-backend/static/js/main.1ddb0f73.chunk.js"
+  },
+  {
+    "revision": "ae92a71578ea6e528772",
+    "url": "/demo-meteo-backend/static/js/runtime-main.62f909d6.js"
+  }
+]);
